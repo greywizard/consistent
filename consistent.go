7f050390398f9284a9eebb01c8f6a1bf88567a94
oskarwojciski <owojciski@greywizard.com>
2017-08-02 15:14:48 +0200

    Hash using xxhash, remove bounded loads feature

--- consistent.go
+++ consistent.go
@@ -7,15 +7,12 @@
 package consistent
 
 import (
-	"encoding/binary"
 	"errors"
 	"fmt"
-	"math"
 	"sort"
 	"sync"
-	"sync/atomic"
 
-	blake2b "github.com/minio/blake2b-simd"
+	"github.com/OneOfOne/xxhash"
 )
 
 const replicationFactor = 10
@@ -24,14 +21,11 @@ var ErrNoHosts = errors.New("no hosts added")
 
 type Host struct {
 	Name string
-	Load int64
 }
 
 type Consistent struct {
 	hosts     map[uint64]string
 	sortedSet []uint64
-	loadMap   map[string]*Host
-	totalLoad int64
 
 	sync.RWMutex
 }
@@ -40,7 +34,6 @@ func New() *Consistent {
 	return &Consistent{
 		hosts:     map[uint64]string{},
 		sortedSet: []uint64{},
-		loadMap:   map[string]*Host{},
 	}
 }
 
@@ -48,11 +41,6 @@ func (c *Consistent) Add(host string) {
 	c.Lock()
 	defer c.Unlock()
 
-	if _, ok := c.loadMap[host]; ok {
-		return
-	}
-
-	c.loadMap[host] = &Host{Name: host, Load: 0}
 	for i := 0; i < replicationFactor; i++ {
 		h := c.hash(fmt.Sprintf("%s%d", host, i))
 		c.hosts[h] = host
@@ -86,38 +74,6 @@ func (c *Consistent) Get(key string) (string, error) {
 	return c.hosts[c.sortedSet[idx]], nil
 }
 
-// It uses Consistent Hashing With Bounded loads
-//
-// https://research.googleblog.com/2017/04/consistent-hashing-with-bounded-loads.html
-//
-// to pick the least loaded host that can serve the key
-//
-// It returns ErrNoHosts if the ring has no hosts in it.
-//
-func (c *Consistent) GetLeast(key string) (string, error) {
-	c.RLock()
-	defer c.RUnlock()
-
-	if len(c.hosts) == 0 {
-		return "", ErrNoHosts
-	}
-
-	h := c.hash(key)
-	idx := c.search(h)
-
-	i := idx
-	for {
-		host := c.hosts[c.sortedSet[i]]
-		if c.loadOK(host) {
-			return host, nil
-		}
-		i++
-		if i >= len(c.hosts) {
-			i = 0
-		}
-	}
-}
-
 func (c *Consistent) search(key uint64) int {
 	idx := sort.Search(len(c.sortedSet), func(i int) bool {
 		return c.sortedSet[i] >= key
@@ -127,42 +83,6 @@ func (c *Consistent) search(key uint64) int {
 		idx = 0
 	}
 	return idx
-}
-
-// Sets the load of `host` to the given `load`
-func (c *Consistent) UpdateLoad(host string, load int64) {
-	c.Lock()
-	defer c.Unlock()
-
-	if _, ok := c.loadMap[host]; !ok {
-		return
-	}
-	c.totalLoad -= c.loadMap[host].Load
-	c.loadMap[host].Load = load
-	c.totalLoad += load
-}
-
-// Increments the load of host by 1
-//
-// should only be used with if you obtained a host with GetLeast
-func (c *Consistent) Inc(host string) {
-	atomic.AddInt64(&c.loadMap[host].Load, 1)
-	atomic.AddInt64(&c.totalLoad, 1)
-
-}
-
-// Decrements the load of host by 1
-//
-// should only be used with if you obtained a host with GetLeast
-func (c *Consistent) Done(host string) {
-	c.Lock()
-	defer c.Unlock()
-
-	if _, ok := c.loadMap[host]; !ok {
-		return
-	}
-	atomic.AddInt64(&c.loadMap[host].Load, -1)
-	atomic.AddInt64(&c.totalLoad, -1)
 }
 
 // Deletes host from the ring
@@ -182,64 +102,10 @@ func (c *Consistent) Remove(host string) bool {
 func (c *Consistent) Hosts() (hosts []string) {
 	c.RLock()
 	defer c.RUnlock()
-	for k, _ := range c.loadMap {
-		hosts = append(hosts, k)
+	for _, host := range c.hosts {
+		hosts = append(hosts, host)
 	}
 	return hosts
-}
-
-// Returns the loads of all the hosts
-func (c *Consistent) GetLoads() map[string]int64 {
-	loads := map[string]int64{}
-
-	for k, v := range c.loadMap {
-		loads[k] = v.Load
-	}
-	return loads
-}
-
-// Returns the maximum load of the single host
-// which is:
-// (total_load/number_of_hosts)*1.25
-// total_load = is the total number of active requests served by hosts
-// for more info:
-// https://research.googleblog.com/2017/04/consistent-hashing-with-bounded-loads.html
-func (c *Consistent) MaxLoad() int64 {
-	if c.totalLoad == 0 {
-		c.totalLoad = 1
-	}
-	var avgLoadPerNode float64
-	avgLoadPerNode = float64(c.totalLoad / int64(len(c.loadMap)))
-	if avgLoadPerNode == 0 {
-		avgLoadPerNode = 1
-	}
-	avgLoadPerNode = math.Ceil(avgLoadPerNode * 1.25)
-	return int64(avgLoadPerNode)
-}
-
-func (c *Consistent) loadOK(host string) bool {
-	// a safety check if someone performed c.Done more than needed
-	if c.totalLoad < 0 {
-		c.totalLoad = 0
-	}
-
-	var avgLoadPerNode float64
-	avgLoadPerNode = float64((c.totalLoad + 1) / int64(len(c.loadMap)))
-	if avgLoadPerNode == 0 {
-		avgLoadPerNode = 1
-	}
-	avgLoadPerNode = math.Ceil(avgLoadPerNode * 1.25)
-
-	bhost, ok := c.loadMap[host]
-	if !ok {
-		panic(fmt.Sprintf("given host(%s) not in loadsMap", bhost.Name))
-	}
-
-	if float64(bhost.Load)+1 <= avgLoadPerNode {
-		return true
-	}
-
-	return false
 }
 
 func (c *Consistent) delSlice(val uint64) {
@@ -251,6 +117,8 @@ func (c *Consistent) delSlice(val uint64) {
 }
 
 func (c *Consistent) hash(key string) uint64 {
-	out := blake2b.Sum512([]byte(key))
-	return binary.LittleEndian.Uint64(out[:])
+	h := xxhash.New64()
+	h.Write([]byte(key))
+	return h.Sum64()
+
 }
--- consistent_test.go
+++ consistent_test.go
@@ -1,9 +1,6 @@
 package consistent
 
-import (
-	"fmt"
-	"testing"
-)
+import "testing"
 
 func TestAdd(t *testing.T) {
 	c := New()
@@ -36,53 +33,6 @@ func TestRemove(t *testing.T) {
 
 	if len(c.sortedSet) != 0 && len(c.hosts) != 0 {
 		t.Fatal(("remove is not working"))
-	}
-
-}
-
-func TestGetLeast(t *testing.T) {
-	c := New()
-
-	c.Add("127.0.0.1:8000")
-	c.Add("92.0.0.1:8000")
-
-	for i := 0; i < 100; i++ {
-		host, err := c.GetLeast("92.0.0.1:80001")
-		if err != nil {
-			t.Fatal(err)
-		}
-		c.Inc(host)
-	}
-
-	for k, v := range c.GetLoads() {
-		if v > c.MaxLoad() {
-			t.Fatalf("host %s is overloaded. %d > %d\n", k, v, c.MaxLoad())
-		}
-	}
-	fmt.Println("Max load per node", c.MaxLoad())
-	fmt.Println(c.GetLoads())
-
-}
-
-func TestIncDone(t *testing.T) {
-	c := New()
-
-	c.Add("127.0.0.1:8000")
-	c.Add("92.0.0.1:8000")
-
-	host, err := c.GetLeast("92.0.0.1:80001")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	c.Inc(host)
-	if c.loadMap[host].Load != 1 {
-		t.Fatalf("host %s load should be 1\n", host)
-	}
-
-	c.Done(host)
-	if c.loadMap[host].Load != 0 {
-		t.Fatalf("host %s load should be 0\n", host)
 	}
 
 }
